8f35aa5557686a050532c29fb07e8ab64c7f709f
Marom Beltran <mrmbeltran@proton.me>
2025-11-08 10:47:49 -0600

    change - rss

--- src/pages/blog/[...slug]/rss.xml.ts
+++ src/pages/blog/[...slug]/rss.xml.ts
@@ -1,4 +1,14 @@
 import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+
+// Generate static paths for all blog posts
+export async function getStaticPaths() {
+  const blogEntries = await getCollection('blog');
+  
+  return blogEntries.map((entry) => ({
+    params: { slug: entry.slug },
+  }));
+}
 
 // Redirect blog post RSS requests to the main RSS feed
 export const GET: APIRoute = async ({ redirect, site, url }) => {
